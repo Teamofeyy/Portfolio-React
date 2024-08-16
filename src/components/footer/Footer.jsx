84d288f1920b3e261fb932fa61ad8b4ddd38c443
Teamofeyy <timarnd06@gmail.com>
2024-08-16 17:58:56 +0300

    Layout transfer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,29 @@
+import vk from "./../../assets/icons/vk.svg"
+import twitter from"./../../assets/icons/twitter.svg"
+import github from "./../../assets/icons/gitHub.svg"
+import instagram from "./../../assets/icons/instagram.svg"
+import linkedin from "./../../assets/icons/linkedIn.svg"
+import './footer.css'
+
+const Footer = () => {
+    return ( 
+    <footer className="footer">
+        <div className="container">
+            <div className="footer__wrapper">
+                 <ul className="social">
+                    <li className="social__item"><a href="#!"><img src={vk} alt="VK" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={instagram} alt="Instagram" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={twitter} alt="Twitter" /></a></li>
+                    <li className="social__item"><a href="https://github.com/Teamofeyy"><img src={github} alt="GitHub" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={linkedin} alt="LinkedIn" /></a></li>
+                 </ul>
+                <div className="copyright">
+                    <p>© 2022 frontend-dev.com</p>
+                </div>
+            </div>
+         </div>
+    </footer>
+     );
+}
+ 
+export default Footer;
