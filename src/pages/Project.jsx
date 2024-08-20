33cf45a3fcf0c8ee0e5c01a5f46e9ca75329f22e
Teamofeyy <timarnd06@gmail.com>
2024-08-20 21:25:56 +0300

    project & button render

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,18 +1,28 @@
+import { useParams } from "react-router-dom";
+import { projects } from "./../helpers/projectsList";
 import ButtonGitHub from "../components/btnGitHub/ButtonGitHub";
-import img from "./../assets/projects/01-big.jpg"
+// import img from "./../assets/projects/01-big.jpg"
 
 
 const Project = () => {
+    const {id} = useParams()
+    const project = projects[id]
+
+
     return ( 
         <main className="section">
             <div className="container">
                 <div className="project-details">
-                    <h1 className="title-1">Video service</h1>
-                    <img src={img} alt="" className="project-details__cover" />
+                    <h1 className="title-1">{project.title}</h1>
+                    <img src={project.imgBig} alt={project.title} className="project-details__cover" />
                     <div className="project-details__desc">
-                        <p>Skills: React, Node.js, MongoDB</p>
+                        <p>Skills: {project.skills}</p>
                     </div>
-                    <ButtonGitHub link="https://github.com/Teamofeyy" />
+
+                    {project.gitHubLink && (
+                        <ButtonGitHub link="https://github.com/Teamofeyy" />
+                    )}
+                    
             </div>
         </div>
     </main>
