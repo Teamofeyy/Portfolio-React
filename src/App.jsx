a99220fe0fd3958392dd3612ff693e0f0b9991eb
Teamofeyy <timarnd06@gmail.com>
2024-08-20 13:28:54 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import Footer from "./components/footer/Footer"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
-// import Contacts from "./pages/Contacts"
-// import Home from "./pages/Home"
-// import Projects from "./pages/Projects"
+import Contacts from "./pages/Contacts"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
 import Project from "./pages/Project"
 import "/styles/main.css"
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      {/* <Home /> */}
-      {/* <Projects /> */}
-      <Project />
-      {/* <Contacts /> */}
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import sun from "./../../assets/icons/sun.svg"
 import moon from "./../../assets/icons/moon.svg"
+import { NavLink } from "react-router-dom"
 import './navbar.css'
 
 const Navbar = () => {
@@ -8,7 +9,9 @@ const Navbar = () => {
         <nav className="nav">
             <div className="container">
                 <div className="nav-row">
-                    <a href="./index.html" className="logo"><strong>Freelancer</strong> portfolio</a>
+                    <NavLink to="/" className="logo">
+                        <strong>Freelancer</strong> portfolio
+                    </NavLink>
 
                     <button className="dark-mode-btn">
                         <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
@@ -16,9 +19,17 @@ const Navbar = () => {
                     </button>
 
                     <ul className="nav-list">
-                        <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                        <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                        <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
+                        <NavLink to="/" className="nav-list__link">
+                            Home
+                        </NavLink>
+
+                        <NavLink to="/projects" className="nav-list__link">
+                            Projects
+                        </NavLink>
+
+                        <NavLink to="/contacts" className="nav-list__link">
+                             Contacts
+                        </NavLink>
                     </ul>
                 </div>
             </div>
