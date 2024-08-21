01e67be9068e73a9a46ba636f83439ce23c6acff
Teamofeyy <timarnd06@gmail.com>
2024-08-21 10:54:27 +0300

    dark theme add

---
+++ src/components/btnDarkMode/BtnDarkMode.jsx
@@ -0,0 +1,35 @@
+import sun from "./sun.svg"
+import moon from "./moon.svg"
+import "./btnDarkMode.css"
+import { useEffect, useRef, useState } from "react"
+
+const BtnDarkMode = () => {
+    const [darkMode, setDarkMode] = useState('light')
+
+    const buttonRef = useRef(null)
+
+    useEffect(() => {
+        if(darkMode === 'dark') {
+            document.body.classList.add('dark')
+            buttonRef.current.classList.add('dark-mode-btn--active')
+        } else {
+            document.body.classList.remove('dark')
+            buttonRef.current.classList.remove('dark-mode-btn--active')
+        }
+    }, [darkMode])
+
+    const toggleDarkMode = () => {
+        setDarkMode((currentValue) => {
+            return currentValue === 'light' ? 'dark' : 'light'
+        })
+    }
+
+    return ( 
+        <button ref={buttonRef} className="dark-mode-btn" onClick={toggleDarkMode}>
+            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+        </button>
+     );
+}
+ 
+export default BtnDarkMode;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
-import sun from "./../../assets/icons/sun.svg"
-import moon from "./../../assets/icons/moon.svg"
 import { NavLink } from "react-router-dom"
+import BtnDarkMode from "../btnDarkMode/BtnDarkMode"
 import './navbar.css'
 
 const Navbar = () => {
@@ -16,10 +15,7 @@ const Navbar = () => {
                         <strong>Freelancer</strong> portfolio
                     </NavLink>
 
-                    <button className="dark-mode-btn">
-                        <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-                        <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-                    </button>
+                    <BtnDarkMode />
 
                     <ul className="nav-list">
                         <NavLink to="/" className={({isActive}) => isActive ? activeLink : normalLink}>
